1e79865ab3b41c6cd789c2ffcd66e01d3188cdca
Balakishore <balakishore996@gmail.com>
2022-12-20 22:23:20 +0530

    init

--- server/server.js
+++ server/server.js
@@ -11,38 +11,38 @@ const cors = require("cors");
 const App = express();
 
 App.use(cors());
-App.use(express.json({limit:"30mb",extended:true}));
-App.use(express.urlencoded({extended:false}));
+App.use(express.json({ limit: "30mb", extended: true }));
+App.use(express.urlencoded({ extended: false }));
 dotenv.config();
 
 let URL = "mongodb+srv://user1:123tester@realestate.4ldww6u.mongodb.net/?retryWrites=true&w=majority"
 // let url ="mongodb://localhost:27017/real_estate"
 
-mongoose.connect(URL,{useNewUrlParser: true},(err)=>{
-    if(!err){
+mongoose.connect(URL, { useNewUrlParser: true }, (err) => {
+    if (!err) {
         console.log("DB Connected");
     }
-    else{
+    else {
         console.log(err);
     }
 });
 
-App.listen(process.env.PORT || 3001,(err)=>{
-    if(!err){
+App.listen(process.env.PORT || 3001, (err) => {
+    if (!err) {
         console.log("server running");
-    }else{
+    } else {
         console.log(err);
     }
 });
 
 
 
-App.get("/",(req,res)=>{
+App.get("/", (req, res) => {
     res.send("Home Page")
 });
 
-App.use("/signup",signupController);
-App.use("/login",loginController);
-App.use("/addProperty",addPropController);
-App.use("/viewProperty",viewPropController);
-App.use("/logout",logoutController);
+App.use("/signup", signupController);
+App.use("/login", loginController);
+App.use("/addProperty", addPropController);
+App.use("/viewProperty", viewPropController);
+App.use("/logout", logoutController);
